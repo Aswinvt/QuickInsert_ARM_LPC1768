8fc8d4c80aa33a1ec43db7798af3751f85400e40
Aswinvt <aswinvt010@gmail.com>
2018-02-19 11:56:35 +0530

    Initial commit

---
+++ LDR Board/split.c
@@ -0,0 +1,30 @@
+/*
+ * File name            : split.c
+ * Compiler             : MDK-Lite
+ * IDE                  : Keil uVision V5
+ * Author               : Etiq Technologies
+ * Processor            : LPC1768
+ * Created on           : November 20, 2017, 01:30 PM
+ * Description          : Program for ADC data conversion
+ */
+ 
+#include "uart.h"
+#include "split.h"
+
+void split(unsigned char s)
+{
+	unsigned char a[4];
+	int x=0;
+
+	while(x<4){
+		a[x] = s%10;
+		s = s/10;
+		x++;
+	}
+	
+	do
+	{
+		--x;
+		UART0_WriteChar(a[x]+48);
+	}while(x);
+}
